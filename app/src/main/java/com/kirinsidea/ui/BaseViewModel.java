f8e9ff25914e064c0364b05b01df83c39281de71
dptmf7705 <sdc01194@gmail.com>
2019-10-02 00:04:00 +0900

    issue-007 로그인 뷰모델 구현

--- app/src/main/java/com/kirinsidea/ui/BaseViewModel.java
+++ app/src/main/java/com/kirinsidea/ui/BaseViewModel.java
@@ -4,15 +4,19 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.lang.ref.WeakReference;
+
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 
-public abstract class BaseViewModel extends ViewModel {
+public abstract class BaseViewModel<N> extends ViewModel {
 
     @NonNull
     private final CompositeDisposable compositeDisposable = new CompositeDisposable();
     @NonNull
     protected final MutableLiveData<Throwable> error = new MutableLiveData<>();
+
+    protected WeakReference<N> navigator;
 
     protected void addDisposable(Disposable disposable) {
         compositeDisposable.add(disposable);
@@ -22,5 +26,9 @@ public abstract class BaseViewModel extends ViewModel {
     protected void onCleared() {
         compositeDisposable.dispose();
         super.onCleared();
+    }
+
+    public void setNavigator(N navigator) {
+        this.navigator = new WeakReference<>(navigator);
     }
 }
---
+++ app/src/main/java/com/kirinsidea/ui/login/LoginViewModel.java
@@ -0,0 +1,76 @@
+package com.kirinsidea.ui.login;
+
+import android.text.TextUtils;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.kirinsidea.data.repository.LoginRepository;
+import com.kirinsidea.ui.BaseViewModel;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+
+public class LoginViewModel extends BaseViewModel<LoginNavigator> {
+    @NonNull
+    private final MutableLiveData<String> inputEmail = new MutableLiveData<>();
+    @NonNull
+    private final MutableLiveData<String> inputPassword = new MutableLiveData<>();
+
+    @NonNull
+    private LoginRepository repository;
+
+    private LoginViewModel(@NonNull final LoginRepository repository) {
+        this.repository = repository;
+    }
+
+    public void startLoginWithGoogle() {
+        addDisposable(repository.observeGoogleLoginIntent()
+                .observeOn(AndroidSchedulers.mainThread())
+                .flatMap(navigator.get()::navigateLoginWithGoogle)
+                .flatMapCompletable(repository::observeLoginWithGoogle)
+                .subscribe(navigator.get()::navigateLoginSuccess,
+                        error::setValue));
+    }
+
+    public void startLoginWithEmail() {
+        // TODO. 이메일 형식 맞는지 체크
+        navigator.get().navigateLoginWithEmail();
+    }
+
+    public void checkPassword() {
+        navigator.get().navigateLoginSuccess();
+    }
+
+    @NonNull
+    public MutableLiveData<String> getInputEmail() {
+        return inputEmail;
+    }
+
+    @NonNull
+    public MutableLiveData<String> getInputPassword() {
+        return inputPassword;
+    }
+
+
+    public static class Factory implements ViewModelProvider.Factory {
+
+        @NonNull
+        private final LoginRepository repository;
+
+        public Factory(@NonNull final LoginRepository repository) {
+            this.repository = repository;
+        }
+
+        @NonNull
+        @Override
+        public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+            if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+                //noinspection unchecked
+                return (T) new LoginViewModel(repository);
+            }
+            throw new IllegalArgumentException("Unknown ViewModel class: " + modelClass.getName());
+        }
+    }
+}
