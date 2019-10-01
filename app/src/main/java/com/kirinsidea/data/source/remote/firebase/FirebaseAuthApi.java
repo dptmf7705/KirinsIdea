a06decb207219c4385493d817ca5b533cdfb7d6b
dptmf7705 <sdc01194@gmail.com>
2019-10-01 23:47:48 +0900

    issue-007 Auth 데이터소스 구현

---
+++ app/src/main/java/com/kirinsidea/data/source/remote/firebase/FirebaseAuthApi.java
@@ -0,0 +1,35 @@
+package com.kirinsidea.data.source.remote.firebase;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+
+import io.reactivex.Completable;
+
+public class FirebaseAuthApi {
+    private static class LazyHolder {
+        private static final FirebaseAuthApi INSTANCE = new FirebaseAuthApi();
+    }
+
+    public static FirebaseAuthApi getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+
+    @NonNull
+    private final FirebaseAuth firebase;
+
+    private FirebaseAuthApi() {
+        this.firebase = FirebaseAuth.getInstance();
+    }
+
+    @NonNull
+    public Completable observeFirebaseAuth(
+            @NonNull final AuthCredential credential) {
+
+        return Completable.create(emitter ->
+                firebase.signInWithCredential(credential)
+                        .addOnSuccessListener(__ -> emitter.onComplete())
+                        .addOnFailureListener(emitter::onError));
+    }
+}
---
+++ app/src/main/java/com/kirinsidea/data/source/remote/google/GoogleLoginApi.java
@@ -0,0 +1,59 @@
+package com.kirinsidea.data.source.remote.google;
+
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.common.api.ApiException;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import io.reactivex.Single;
+
+public class GoogleLoginApi {
+    private volatile static GoogleLoginApi INSTANCE = null;
+
+    public static GoogleLoginApi getInstance(@NonNull final GoogleSignInClient signInClient) {
+        if (INSTANCE == null) {
+            synchronized (GoogleLoginApi.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new GoogleLoginApi(signInClient);
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    @NonNull
+    private GoogleSignInClient signInClient;
+
+    private GoogleLoginApi(@NonNull final GoogleSignInClient signInClient) {
+        this.signInClient = signInClient;
+    }
+
+    @NonNull
+    public Single<Intent> observeLoginIntent() {
+        return Single.just(signInClient.getSignInIntent());
+    }
+
+    public Single<AuthCredential> observeAuthCredential(@NonNull final Intent data) {
+        return Single.create(emitter -> {
+            try {
+                final GoogleSignInAccount account = GoogleSignIn
+                        .getSignedInAccountFromIntent(data)
+                        .getResult(ApiException.class);
+
+                if (account != null && account.getIdToken() != null) {
+                    emitter.onSuccess(GoogleAuthProvider
+                            .getCredential(account.getIdToken(), null));
+                }
+            } catch (ApiException e) {
+                emitter.onError(e);
+            }
+        });
+
+    }
+}
