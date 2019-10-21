411b1c5c9af0b69bf7a0503d43f4917122e62b00
dptmf7705 <sdc01194@gmail.com>
2019-10-21 18:47:18 +0900

    #19 Retrofit API - URL string 상수 수정

--- app/src/main/java/com/kirinsidea/common/Constants.java
+++ app/src/main/java/com/kirinsidea/common/Constants.java
@@ -2,22 +2,6 @@ package com.kirinsidea.common;
 
 public abstract class Constants {
 
-    public @interface Retrofit {
-        /* 서버의 base URL */
-        String BASE_URL = "http://133.186.146.239:5000/";
-
-        /* 새 북마크 추가 URL */
-        String SUB_URL_NEW_BOOKMARK = "webclipp";
-
-        /* 폴더 생성 URL */
-        String SUB_URL_NEW_FOLDER = "folder";
-    }
-
-    public @interface Room {
-        /* Room Database Name */
-        String DATABASE_NAME = "app_database";
-    }
-
     public @interface Message {
         /* init view model 에러 메시지 */
         String ERROR_INIT_VIEW_MODEL = "Unknown ViewModel class";
--- app/src/main/java/com/kirinsidea/data/source/remote/kirin/RetrofitClient.java
+++ app/src/main/java/com/kirinsidea/data/source/remote/kirin/RetrofitClient.java
@@ -3,7 +3,7 @@ package com.kirinsidea.data.source.remote.kirin;
 import androidx.annotation.NonNull;
 
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import com.kirinsidea.common.Constants;
+import com.kirinsidea.BuildConfig;
 import com.kirinsidea.data.source.remote.kirin.api.BookmarkApi;
 import com.kirinsidea.data.source.remote.kirin.api.FileApi;
 import com.kirinsidea.data.source.remote.kirin.api.FolderApi;
@@ -11,17 +11,21 @@ import com.kirinsidea.data.source.remote.kirin.request.NewBookmarkRequest;
 import com.kirinsidea.data.source.remote.kirin.request.NewFolderRequest;
 import com.kirinsidea.data.source.remote.kirin.response.NewBookmarkResponse;
 
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Completable;
 import io.reactivex.Single;
 import io.reactivex.schedulers.Schedulers;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
 import okhttp3.ResponseBody;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Url;
 
 public class RetrofitClient {
+    private static final String BASE_URL = "http://133.186.146.239:5000/";
+
     private static class LazyHolder {
         private static final RetrofitClient INSTANCE = new RetrofitClient();
     }
@@ -35,21 +39,55 @@ public class RetrofitClient {
     private final Retrofit retrofit;
 
     private RetrofitClient() {
-        // TODO header 에 유저 아이디 추가
-        final OkHttpClient httpClient = new OkHttpClient.Builder()
-                .addInterceptor(chain -> {
-                    final Request.Builder builder = chain.request().newBuilder();
-//                        .header();
-                    return chain.proceed(builder.build());
-                })
-                .build();
+        final OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
+
+        setHeader(httpClientBuilder);
+        setLogger(httpClientBuilder);
+        setErrorHandler(httpClientBuilder);
+        setTimeout(httpClientBuilder);
 
         retrofit = new Retrofit.Builder()
-                .baseUrl(Constants.Retrofit.BASE_URL)
+                .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .client(httpClient)
+                .client(httpClientBuilder.build())
                 .build();
+    }
+
+    /**
+     * Header 설정
+     */
+    private void setHeader(OkHttpClient.Builder builder) {
+        builder.addInterceptor(chain ->
+                chain.proceed(chain.request().newBuilder()
+//                        .header("UserToken", "")
+                        .build()));
+    }
+
+    /**
+     * Logger 설정
+     */
+    private void setLogger(OkHttpClient.Builder builder) {
+        if (BuildConfig.DEBUG) {
+            builder.addInterceptor(new HttpLoggingInterceptor()
+                    .setLevel(HttpLoggingInterceptor.Level.BODY));
+        }
+    }
+
+    /**
+     * ErrorHandler 설정
+     */
+    private void setErrorHandler(OkHttpClient.Builder builder) {
+        // TODO custom error handling
+    }
+
+    /**
+     * Timeout 설정
+     */
+    private void setTimeout(OkHttpClient.Builder builder) {
+        builder.connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS);
     }
 
     @NonNull
