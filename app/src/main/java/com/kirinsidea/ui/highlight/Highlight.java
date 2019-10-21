2b7d334065e5a4f368a4cf36d4e4073c6ee9bfa3
dptmf7705 <sdc01194@gmail.com>
2019-10-22 04:02:04 +0900

    #3 하이라이트 레파지토리 인터페이스 작성

--- app/src/main/java/com/kirinsidea/ui/highlight/Highlight.java
+++ app/src/main/java/com/kirinsidea/ui/highlight/Highlight.java
@@ -2,6 +2,8 @@ package com.kirinsidea.ui.highlight;
 
 import androidx.annotation.NonNull;
 
+import com.kirinsidea.data.source.local.room.entity.HighlightEntity;
+
 public class Highlight {
     private final int id;
     private final int bookmarkId;
@@ -22,6 +24,15 @@ public class Highlight {
         this.endIndex = endIndex;
         this.text = text;
         this.color = color;
+    }
+
+    private Highlight(@NonNull final Builder builder) {
+        this.id = builder.id;
+        this.bookmarkId = builder.bookmarkId;
+        this.startIndex = builder.startIndex;
+        this.endIndex = builder.endIndex;
+        this.text = builder.text;
+        this.color = builder.color;
     }
 
     public int getId() {
@@ -62,5 +73,29 @@ public class Highlight {
 
     public void setColor(HighlightColor color) {
         this.color = color;
+    }
+
+    public static class Builder {
+        private final int id;
+        private final int bookmarkId;
+        private final int startIndex;
+        private final int endIndex;
+        @NonNull
+        private final String text;
+        @NonNull
+        private final HighlightColor color;
+
+        public Builder(@NonNull final HighlightEntity entity) {
+            this.id = entity.getId();
+            this.bookmarkId = entity.getBookmarkId();
+            this.startIndex = entity.getStartIndex();
+            this.endIndex = entity.getEndIndex();
+            this.text = entity.getText();
+            this.color = HighlightColor.valueOf(entity.getColor());
+        }
+
+        public Highlight build() {
+            return new Highlight(this);
+        }
     }
 }
