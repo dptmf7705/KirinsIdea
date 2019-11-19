f32ccfc6c39443553df9286533aa500b20b5d681
soyeon <3937839p@gmail.com>
2019-11-19 21:54:22 +0900

    #48 테스트 api 수정

--- app/src/main/java/com/kirinsidea/data/source/remote/kirin/mock/BookmarkMockApi.java
+++ app/src/main/java/com/kirinsidea/data/source/remote/kirin/mock/BookmarkMockApi.java
@@ -10,6 +10,7 @@ import com.kirinsidea.ui.bookmarklist.BookmarkItem;
 import com.kirinsidea.utils.DateUtil;
 
 import java.util.Date;
+import java.util.Random;
 
 import io.reactivex.Single;
 
@@ -17,7 +18,7 @@ public class BookmarkMockApi implements BookmarkApi {
 
     @Override
     public Single<SingleResponse<BookmarkEntity>> addNewBookmark(NewBookmarkRequest request) {
-        BookmarkEntity bookmark = new BookmarkEntity.Builder(new Bookmark("0", "https://search.naver.com/search.naver?where=nexearch&query=%EB%8B%A5%ED%84%B0%ED%93%A8%EB%A6%AC+%EB%AF%B8%EC%84%B8%EB%A8%BC%EC%A7%80+%EB%A7%88%EC%8A%A4%ED%81%AC&sm=top_lve&ie=utf8"
+        BookmarkEntity bookmark = new BookmarkEntity.Builder(new Bookmark("1", "https://search.naver.com/search.naver?where=nexearch&query=%EB%8B%A5%ED%84%B0%ED%93%A8%EB%A6%AC+%EB%AF%B8%EC%84%B8%EB%A8%BC%EC%A7%80+%EB%A7%88%EC%8A%A4%ED%81%AC&sm=top_lve&ie=utf8"
                 , "https://naver.com", "얄루", "지은이", "2019-10-25 20:46:00", "<html>\n" +
                 "<head>\n" +
                 "  <title>HTML - 수업소개</title>\n" +
@@ -37,8 +38,8 @@ public class BookmarkMockApi implements BookmarkApi {
                 "본 수업을 효과적으로 수행하기 위해서는 웹애플리케이션에 대한 전반적인 이해가 필요합니다. 이를 위해서 준비된 수업은 아래 링크를 통해서 접근하실 수 있습니다. \n" +
                 "</body>\n" +
                 "</html>")).build();
-        BookmarkEntity bookmarkItem = new BookmarkEntity.Builder(new BookmarkItem("0",
-                "https://naver.com", "https://adcr.naver.com/adcr?x=fVmUp2v/RDLhlzjzLtKnQf///w==kSANdnnR+Rhnm1B+R0hXTqKRjazn6K1HmsXxoCUllpHirSVZEI/kS4ct/FdLTmWxco7grlnH5+iW7/FlsKeR4DWyDRGRo6OOWXmG6vA/9MOg8z8ejSh59zREambBoHV3DKbAfiHVwkJ2xz6gLCCvJXCeXdfepqwbvBF4k/rI8DJQvEMMIWCtTnaXiCBgYtgsG6nW93HG+7ukuGIDjVP0CsNIX8+lf1Dr53ZTYip9OsdtST8aJblu7CcWYljnAHCy9C6zPFplg17OKVIAtTqNIdx8SmkgoLENKdHvzBgSag+jHD1FGf9YP9+Q5Z8WODoNfthZ714qhctTUWeXwqe7OvWnq+b/T0cbEQwGMoLGmdLosrjleh1RgA16fJgf3m4zsQ+rAC8X8t6E1Y9dokd9U/oPtUsVBHppXnDDswhTQgKKZdc+BHpFivW+lk3bQMJAsHiy681D/NTS3eW/INDSWC4Z2d+LgR8sBLhK2JOfvRvcssaqq2KFzaArwoVg+LTkLGXskj8naJHIKuaIaLt/e1IZ+pNZWkmT1+zXVV6SQNaI/hJTnXL8A1E49Hdvz4ig+rpDOIbNhhlj7fSBEj61vaA5sfO2rtuNcuSepRtlOaCJCuxmrgozEVClvN3ZpQWHS"
+        BookmarkEntity bookmarkItem = new BookmarkEntity.Builder(new BookmarkItem(String.valueOf(new Random().nextInt()),
+                "https://naver.com", "http://www.newsian.co.kr/news/photo/201908/36256_15176_2729.png"
                 , "얄루", DateUtil.getCurrentDateTime(), request.getFolderId())).build();
         SingleResponse<BookmarkEntity> response = new SingleResponse<>(RetrofitResultCode.SUCCESS, bookmark);
         SingleResponse<BookmarkEntity> response1 = new SingleResponse<>(RetrofitResultCode.SUCCESS, bookmarkItem);
